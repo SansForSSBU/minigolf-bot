1aaf909608f1d9ab52e6d110585d39750c2cd0bf
Viibrant <11376396+Viibrant@users.noreply.github.com>
2025-08-17 03:10:33 +0100

    refactor(constants): clean up DEFAULT_MOVES typing and add thresholds

--- src/minigolf/constants.py
+++ src/minigolf/constants.py
@@ -5,13 +5,13 @@ DEFAULT_ELASTICITY = 0.4
 DEFAULT_FLOOR_FRICTION = 0.8
 SIMULATION_SPEED = 3.0
 BALL_MOMENT = 1.0
-DEFAULT_MOVES = list(
-    reversed(
-        [
-            [Vec2d(300.0, 0.0), 10000.0],
-            [Vec2d(0.0, -300.0), 10000.0],
-            [Vec2d(-300.0, 0.0), 10000.0],
-            [Vec2d(-15.0, 500.0), 10000.0],
-        ]
-    )
+DEFAULT_MOVES: list[list[Vec2d | float]] = list(
+    [
+        [Vec2d(300.0, 0.0), 10000.0],
+        [Vec2d(0.0, -300.0), 10000.0],
+        [Vec2d(-300.0, 0.0), 10000.0],
+        [Vec2d(-15.0, 500.0), 10000.0],
+    ]
 )
+STOPPING_VELOCITY: float = 10.0
+VELOCITY_THRESHOLD: float = 50.0
